fa70d6f9b92b846c39af1970947569239de4355c
Omaroa <omaroaing@gmail.com>
2023-09-05 16:57:47 -0500

    V002

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from './components/Skills';
+
+
 
 function App() {
   return (
-    <div className="App">
-      Omar
+    <div>
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,36 @@
+export const projects = [
+    {
+      title: "React Reserve",
+      subtitle: "MERN Stack",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./img/lista-tareas.png",
+      link: "https://i.postimg.cc/bJfnyc1B/2.jpg",
+    },
+    {
+      title: "React Tracks",
+      subtitle: "React and Python",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-2.gif",
+      link: "https://i.postimg.cc/bJfnyc1B/2.jpg",
+    },
+    {
+      title: "DevChat",
+      subtitle: "React and Firebase",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-3.gif",
+      link: "https://i.postimg.cc/bJfnyc1B/2.jpg",
+    },
+    {
+      title: "Epic Todo App",
+      subtitle: "React Hooks",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-4.gif",
+      link: "https://i.postimg.cc/bJfnyc1B/2.jpg",
+    },
+  ];
+
+  export let skills=["omar","jacier"];
